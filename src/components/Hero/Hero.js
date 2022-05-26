e0d225e1b0832d38b02f311ac59e36af5c1425ac
Viktor Svensson <42525041+viktorsve@users.noreply.github.com>
2022-05-26 14:37:26 +0200

    Add fonts and image optimization

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source
+          type='image/avif'
+          srcSet='
+            /images/hero-img.avif 1x,
+            /images/hero-img@2x.avif 2x,
+            /images/hero-img@3x.avif 3x
+          '
+        />
+        <source
+          type='image/jpeg'
+          srcSet='
+            /images/hero-img.jpg 1x,
+            /images/hero-img@2x.jpg 2x,
+            /images/hero-img@3x.jpg 3x
+          '
+        />
+        <HeroImage
+          alt='An example photo, cat on black background'
+          src='/images/hero-img.jpg'
+        />
+      </picture>
+      <Swoop alt=' ' src='/swoop.svg' />
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.section`
   position: relative;
@@ -18,14 +39,14 @@ const Wrapper = styled.section`
   justify-content: center;
   align-items: flex-end;
   background: hsl(0deg 0% 1%);
-`;
+`
 
 const HeroImage = styled.img`
   display: block;
   width: 500px;
   height: 500px;
   max-height: 100%;
-`;
+`
 
 const Swoop = styled.img`
   position: absolute;
@@ -37,6 +58,6 @@ const Swoop = styled.img`
   */
   bottom: -2px;
   width: 100%;
-`;
+`
 
-export default Hero;
+export default Hero
--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type='image/avif'
+            srcSet={`
+              ${src.replace('.jpg', '.avif')} 1x,
+              ${src.replace('.jpg', '@2x.avif')} 2x,
+              ${src.replace('.jpg', '@3x.avif')} 3x
+            `}
+          />
+          <source
+            type='image/jpeg'
+            srcSet={`
+              ${src} 1x,
+              ${src.replace('.jpg', '@2x.jpg')} 2x,
+              ${src.replace('.jpg', '@3x.jpg')} 3x
+            `}
+          />
+          <Image alt={alt} src={src} />
+        </picture>
       </Anchor>
       <Tags>
-        {tags.map((tag) => (
+        {tags.map(tag => (
           <Tag key={tag}>{tag}</Tag>
         ))}
       </Tags>
     </article>
-  );
-};
+  )
+}
 
 const Anchor = styled.a`
   text-decoration: none;
   color: inherit;
   outline-offset: 4px;
-`;
+`
 
 const Image = styled.img`
   display: block;
@@ -28,20 +46,29 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
-`;
+
+  object-fit: cover;
+`
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
-`;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding: 4px 0px;
+`
 
 const Tag = styled.li`
+  display: inline;
+  margin-right: 8px;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
-`;
 
-export default PhotoGridItem;
+  &:not(:last-of-type) {
+    margin-right: 8px;
+  }
+`
+
+export default PhotoGridItem
